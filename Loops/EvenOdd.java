45183005c5b5127212473f88392c7578e8f5330a
neha-chauhan-nc <nehachauhan2807.r@gmail.com>
2026-02-25 17:15:28 +0000

    Even odd number programm

---
+++ Loops/EvenOdd.java
@@ -0,0 +1,11 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        for(int i=0; i<=20; i++){
+            if (i%2==0) {
+                System.out.println("Even number: "+ i);
+            } else {
+                System.out.println("Odd number: " + i);
+            }
+        }
+    }
+}
