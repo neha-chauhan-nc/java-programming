9d266f3cb5a8237f7032661371acd6421d788362
neha-chauhan-nc <nehachauhan2807.r@gmail.com>
2026-02-25 16:32:38 +0000

    DoWhileSequence Number Program

---
+++ Loops/DoWhileSequenceNo.java
@@ -0,0 +1,11 @@
+public class DoWhileSequenceNo{
+    public static void main(String[] args) {
+        
+        int i=1;
+        do {
+            System.out.println("Number is: "+ i);
+            i++;
+        } while (i<=10);
+    }
+
+}
