23571d266aa99b8087070da5a45fbfaf34abf2f3
neha-chauhan-nc <nehachauhan2807.r@gmail.com>
2026-02-25 16:09:24 +0000

    PrintNo. program

---
+++ Loops/PrintNo.java
@@ -0,0 +1,8 @@
+public class PrintNo {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+            System.out.println("Number is : "+ i);
+        }
+    }
+    
+}
