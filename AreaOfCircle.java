8dad7ad621b46dfda1c78a35cf19915a6b6f444b
neha-chauhan-nc <nehachauhan2807.r@gmail.com>
2026-07-09 23:28:26 +0530

    Add files via upload

---
+++ AreaOfCircle.java
@@ -0,0 +1,14 @@
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        int r=5;
+        System.out.println(3.141592*r*r);//o/p:- double*int= double (priority:-double>int)
+
+
+        //OR
+
+        double rad=3.5;
+        double a=3.141592*rad*rad;
+        System.out.println(a);
+    }
+    
+}
