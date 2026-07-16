a0e74151836a9af1aea57ea4ef9eee1eff1eaf39
neha-chauhan-nc <nehachauhan2807.r@gmail.com>
2026-07-16 23:05:40 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+    public static void main(String[] args) {
+        int x =5;//both initialize & declare
+        System.out.print(x+ " ");//x with space print, no println used only print used., o/p:-5 _(space)
+
+        int y; //declare
+        y=20; //initialize
+        System.out.println(y);//o/p: 5 20
+        System.out.println(x+"neha");//x ke aage append , o/p:-5neha
+        System.out.println(x+"3");//joint , o/p:-53
+        System.out.println(x+y); //o/p:- 25
+
+
+        x=7;// re-initialize or update
+        System.out.println(x);
+
+    }
+}
