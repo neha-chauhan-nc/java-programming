e11ef512a42ae5e9057b6ab0da4447fb59887873
neha-chauhan-nc <nehachauhan2807.r@gmail.com>
2026-07-17 22:04:50 +0530

    Add files via upload

---
+++ arithmeticOperationsOnInt.java
@@ -0,0 +1,16 @@
+public class arithmeticOperationsOnInt {
+public static void main(String[] args) {
+    int x=39, y =10;
+
+    System.out.println(x+y);
+    System.out.println(x-y);
+    System.out.println(x*y);
+    System.out.println(x/y);//o.p:3 coz: int/int=int
+
+    double a=10,b=5; //double stores realno. & decimal no.
+    System.out.println(a+b);
+    System.out.println(a-b);
+    System.out.println(a*b);
+    System.out.println(a/b);
+}    
+}
