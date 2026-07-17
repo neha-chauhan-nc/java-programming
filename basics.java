97d7a627cec2d66871b6cfefceb628ec1e5bb6ca
neha-chauhan-nc <nehachauhan2807.r@gmail.com>
2026-07-17 22:04:27 +0530

    Add files via upload

---
+++ basics.java
@@ -0,0 +1,11 @@
+
+public class basics {
+    public static void main(String[] args) {
+        System.out.println(" ");//space(" ")print
+        System.out.println("hello");
+        System.out.println("hello neha\n and khushi");
+        System.out.println("70");//string/text
+        System.out.println(70+1);//number
+    }
+    
+}
